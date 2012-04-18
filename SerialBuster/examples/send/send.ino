916cedd4f1b7d2a85a1b8190b39512c0ba094279
Mattias Gunneras <mattias@breakfastny.com>
2012-04-18 16:37:32 -0400

    Sending real packet for testing

--- SerialBuster/examples/send/send.ino
+++ SerialBuster/examples/send/send.ino
@@ -1,28 +1,28 @@
 #include "SerialBuster.h"
 #include "Buffer.h"
 
-SerialBuster sb = SerialBuster(512, 512);
+SerialBuster sb = SerialBuster(256, 256, 128);
 long mover = 0;
 
 void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
-  sb.init(115200);
+  sb.init(9600);
   //sb.setCallback(incomingPacket);
   delay(50);
 }
 
 void loop() {
   sb.update();
-  if(mover++ % 4000 == 0) {
+  if(mover++ % 10000 == 0) {
 
-    const uint8_t * payload = (const uint8_t *)"Hello from Arduino.";
-    sb.sendPacket(254, payload, 19);
-
-    int a = availableMemory();
-    float percent = ((float)a) / 2048.0;
-    Serial.print("mem: ");
-    Serial.println(percent * 100);
+    const uint8_t * payload = (const uint8_t *)"An end (\x03) char!";
+    sb.sendPacket(255, payload, strlen((const char *)payload));
+    
+//    int a = availableMemory();
+//    float percent = ((float)a) / 2048.0;
+//    Serial.print("mem: ");
+//    Serial.println(percent * 100);
   }
 }
 
